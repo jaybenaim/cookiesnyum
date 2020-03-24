8d59ff4a684e5c50a0adaf928961dd2af72524cb
jaybenaim <benaimjacob@gmail.com>
2020-03-24 18:08:17 -0400

    added custom slider fn for desktop

--- src/components/Home/FeaturedProductsSlider/index.jsx
+++ src/components/Home/FeaturedProductsSlider/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import cookie1 from "../../../assets/images/cookie1.jpg";
 import cookie2 from "../../../assets/images/cookie2.jpg";
 import cookie3 from "../../../assets/images/cookie3.jpg";
-import "../../../assets/stylesheets/featuredSlider.css";
+import "../../../assets/stylesheets/featuredProductsSlider.css";
 
 const FeaturedSlider = () => {
   const defaultDisplay = [
@@ -39,6 +39,35 @@ const FeaturedSlider = () => {
       }
     }
   };
+  const displayAllCards = keyValue => {
+    return defaultDisplay.map((card, i) => {
+      const key = {
+        0: "cookies",
+        1: "scones",
+        2: "biscotti"
+      };
+      const keyCode = key[keyValue];
+
+      return (
+        <div class="card" key={i}>
+          <img
+            className="card-img"
+            src={card[keyCode].src}
+            alt={card[keyCode].alt}
+            height={220}
+            width={120}
+          />
+          <div className="card-body">
+            <h3>{card[keyCode].text}</h3>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      );
+    });
+  };
   const displayCard = () => {
     const key = {
       0: "cookies",
@@ -49,16 +78,23 @@ const FeaturedSlider = () => {
     const { text, src, alt } = defaultDisplay[0][keyCode];
     return (
       <div class="card">
+        <span className="prev round" onClick={() => getCard("prev")}>
+          &#8249;
+        </span>
         <img
-          className="card-image"
+          className="card-img"
           src={src}
           alt={alt}
           height={320}
           width={"80%"}
         />
-        <div class="card-body">
+        <span className="next round" onClick={() => getCard("next")}>
+          &#8250;
+        </span>
+
+        <div className="card-body">
           <h3>{text}</h3>
-          <p class="card-text">
+          <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
@@ -69,13 +105,12 @@ const FeaturedSlider = () => {
   return (
     <>
       <div>
-        {displayCard()}hello
-        <span className="" onClick={() => getCard("prev")}>
-          Previous
-        </span>
-        <span className="" onClick={() => getCard("next")}>
-          Next
-        </span>
+        <div className="show-for-mobile">{displayCard()}</div>
+        <div className="show-for-desktop">
+          {displayAllCards(0)}
+          {displayAllCards(1)}
+          {displayAllCards(2)}
+        </div>
       </div>
     </>
   );
