623ff06de14b16a767bcd406ae092f2c118b33e9
jaybenaim <benaimjacob@gmail.com>
2020-03-24 01:24:46 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+﻿import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Home from "./components/Home";
+
+//TODO Web Template Studio: Add routes for your new pages here.
+const App = () => {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = { Home } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+}
+
+export default App;
