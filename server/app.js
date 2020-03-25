4da3a4531398948b23d026a449bef941b3fe8db3
jaybenaim <benaimjacob@gmail.com>
2020-03-25 14:58:43 -0400

    removed react build

--- server/app.js
+++ server/app.js
@@ -62,12 +62,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, "build")));
+// app.use(express.static(path.resolve(__dirname, "build")));
 
 app.use("/api", indexRouter);
-app.get("*", (req, res) => {
-  res.sendFile("build/index.html", { root: __dirname });
-});
+// app.get("*", (req, res) => {
+//   res.sendFile("build/index.html", { root: __dirname });
+// });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
