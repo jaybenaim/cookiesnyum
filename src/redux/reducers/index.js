80874c132b37813709de18a3de576f34900b4f4f
jaybenaim <benaimjacob@gmail.com>
2020-03-27 10:39:49 -0400

    reducers conflict

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,18 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducers";
 import errorReducer from "./errorReducers";
-<<<<<<< HEAD
-import { CartReducers } from "react-cart-components";
-=======
 import cartReducers from "./cartReducers";
->>>>>>> 27922822caa3a190b566351cc7b4c2227e80093a
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-<<<<<<< HEAD
-  cart: CartReducers
-=======
   cart: cartReducers
->>>>>>> 27922822caa3a190b566351cc7b4c2227e80093a
 });
