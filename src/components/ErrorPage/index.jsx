f56c2047a7eb781966c0b0bfc15c4c820f1ec70c
jaybenaim <benaimjacob@gmail.com>
2020-03-28 01:15:21 -0400

    new error page

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import "../../assets/stylesheets/errorPage.css";
 const ErrorPage = () => {
   return (
-    <div className="error-page">
-      <div className="container">
-        <div className="heading">OOPS!</div>
-        <div className="text">Something went wrong!</div>
-        <a href="/">Return back to greatness</a>
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>404</h1>
+        </div>
+        <h2>Oops, The Page you are looking for can't be found!</h2>
+
+        <a href="/">
+          <span class="arrow"></span>Return To Homepage
+        </a>
       </div>
     </div>
   );
