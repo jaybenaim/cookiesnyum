218321033d2a47617941de8d70bef0b4bb9b9a04
jaybenaim <benaimjacob@gmail.com>
2020-04-03 00:57:15 -0400

    show more created for about us info

--- src/components/Home/AboutUs/index.jsx
+++ src/components/Home/AboutUs/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../assets/stylesheets/aboutUs.css";
 import profile from "../../../assets/images/aboutUs/profile.png";
 import xoxo from "../../../assets/images/xoxo.png";
+import { Button } from "react-materialize";
 
 const AboutUs = () => {
+  const [more, showMore] = useState(false);
   return (
     <>
       <div className="about-us-grid desktop-grid mobile-grid">
@@ -17,28 +19,48 @@ const AboutUs = () => {
           </h3>
 
           <p>
-            DolceNadaa Began in early 2020 with mother daughter duo Nada {"&"}
-            Ashley Aceto. Baking has always been a passion of ours since I can
-            remember! Our favourite thing to make together was warm gooey
-            chocolate chip cookies and then it came to us STUFFED COOKIES. We
-            decided that everyone loves cookies and warm cookies always manage
-            to bring you back to your childhood, so we wanted to provide
-            customers with an experience. This experience is to bring you back
-            to when you were little a kid and walked into the kitchen to smell
-            the baking aromas and you quickly tried to look into the oven to see
-            what was baking. We decided to mainly focus on a wide variety of
-            stuffed cookies with our brown butter chocolate chip cookie dough
-            base and then we decided to  stuff our cookies with some of our
-            favourite chocolate bars, baked goods and so much more. Our cookies
-            are crunchy on the outside and warm and gooey on the inside. Some of
-            our favourite stuffed creations include Oreo Birthday Cake, Peanut
-            Butter Dream Cup, Nutella goddess and Double Dolce Delight! Aside
-            from our stuffed cookies we have Mary Neves who specializes in our
-            amazingly soft and fluffy scones. Our scones are uniquely hand
-            crafted and simply delicious, we offer Blueberry, Cinnamon bun,
-            Orange Cranberry and Chocolate chip. We can't wait for you to
-            experience DolceNadaa for yourself {"<3"} - XOXO Your Favourite
-            Dolce Girls{" "}
+            {more ? (
+              <>
+                <span>
+                  This experience is to bring you back to when you were little a
+                  kid and walked into the kitchen to smell the baking aromas and
+                  you quickly tried to look into the oven to see what was
+                  baking. We decided to mainly focus on a wide variety of
+                  stuffed cookies with our brown butter chocolate chip cookie
+                  dough base and then we decided to  stuff our cookies with some
+                  of our favourite chocolate bars, baked goods and so much more.
+                  Our cookies are crunchy on the outside and warm and gooey on
+                  the inside. Some of our favourite stuffed creations include
+                  Oreo Birthday Cake, Peanut Butter Dream Cup, Nutella
+                  goddess and Double Dolce Delight! Aside from our stuffed
+                  cookies we have Mary Neves who specializes in our amazingly
+                  soft and fluffy scones. Our scones are uniquely hand crafted
+                  and simply delicious, we offer Blueberry, Cinnamon bun, Orange
+                  Cranberry and Chocolate chip. We can't wait for you to
+                  experience DolceNadaa for yourself {"<3"}
+                   - XOXO Your Favourite Dolce Girls{" "}
+                </span>
+                <Button node="button" onClick={() => showMore(!more)}>
+                  Show Less
+                </Button>
+              </>
+            ) : (
+              <>
+                <span>
+                  DolceNadaa Began in early 2020 with mother daughter duo Nada{" "}
+                  {"&"}
+                  Ashley Aceto. Baking has always been a passion of ours since I
+                  can remember! Our favourite thing to make together was warm
+                  gooey chocolate chip cookies and then it came to us STUFFED
+                  COOKIES. We decided that everyone loves cookies and warm
+                  cookies always manage to bring you back to your childhood, so
+                  we wanted to provide customers with an experience.
+                </span>
+                <Button node="button" onClick={() => showMore(!more)}>
+                  Show More
+                </Button>
+              </>
+            )}
           </p>
           <img src={xoxo} alt="slogan" height={80} width={"40%"} />
         </div>
