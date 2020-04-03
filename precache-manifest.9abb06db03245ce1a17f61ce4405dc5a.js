52811a9ae4f6d5a7c542d1d0676bc6db983de682
jaybenaim <benaimjacob@gmail.com>
2020-04-03 12:22:21 -0400

    Updates

---
+++ precache-manifest.9abb06db03245ce1a17f61ce4405dc5a.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5767e129231ab0e28e8ced1377a18918",
+    "url": "/dolcenadaa/index.html"
+  },
+  {
+    "revision": "9b5556fa1e7f4f891439",
+    "url": "/dolcenadaa/static/css/2.0d4493d6.chunk.css"
+  },
+  {
+    "revision": "87963f1a008a3753cd79",
+    "url": "/dolcenadaa/static/css/main.8ebc6c51.chunk.css"
+  },
+  {
+    "revision": "9b5556fa1e7f4f891439",
+    "url": "/dolcenadaa/static/js/2.f016f02d.chunk.js"
+  },
+  {
+    "revision": "2a0b20e1d02e77e8b582ff21a215748a",
+    "url": "/dolcenadaa/static/js/2.f016f02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87963f1a008a3753cd79",
+    "url": "/dolcenadaa/static/js/main.a7f77c19.chunk.js"
+  },
+  {
+    "revision": "09b1ed9f15cf7a4a0879",
+    "url": "/dolcenadaa/static/js/runtime-main.0baa157b.js"
+  },
+  {
+    "revision": "fac9e2fe2196dc7691b34ac58e2b76ba",
+    "url": "/dolcenadaa/static/media/amertti.fac9e2fe.png"
+  },
+  {
+    "revision": "3291cbec4afbe557d4e987f47f0f120b",
+    "url": "/dolcenadaa/static/media/assorted.3291cbec.jpg"
+  },
+  {
+    "revision": "56e572873c0722687877461c67da969c",
+    "url": "/dolcenadaa/static/media/bananaBread.56e57287.png"
+  },
+  {
+    "revision": "fd7962331b3943daf8706dd12241f539",
+    "url": "/dolcenadaa/static/media/blueberry.fd796233.png"
+  },
+  {
+    "revision": "2db80bb01252000db0c46f3256f9fc37",
+    "url": "/dolcenadaa/static/media/chocolate.2db80bb0.png"
+  },
+  {
+    "revision": "9460bb3202ce00e560c6407c4c908c2a",
+    "url": "/dolcenadaa/static/media/cinnamonbun.9460bb32.png"
+  },
+  {
+    "revision": "e383d778ce55f026a7e2f50de0991752",
+    "url": "/dolcenadaa/static/media/cookieButter.e383d778.png"
+  },
+  {
+    "revision": "b2b7b050913dfcf01643a1c8597edde6",
+    "url": "/dolcenadaa/static/media/cranberryorange.b2b7b050.png"
+  },
+  {
+    "revision": "9b4de89e4e230f7b2ed6ba57a85765a2",
+    "url": "/dolcenadaa/static/media/doubleDolceDelight.9b4de89e.png"
+  },
+  {
+    "revision": "2d7a5cac12f138fcedc440224d6a3777",
+    "url": "/dolcenadaa/static/media/dozen.2d7a5cac.jpg"
+  },
+  {
+    "revision": "093464a2cd077df3087b5260a5d954e5",
+    "url": "/dolcenadaa/static/media/easter.093464a2.JPG"
+  },
+  {
+    "revision": "e355476c682e48a5a9fc2d9cd6d0142e",
+    "url": "/dolcenadaa/static/media/everythingCookie.e355476c.png"
+  },
+  {
+    "revision": "fe76112c934a777e143fd0a0c5e58c66",
+    "url": "/dolcenadaa/static/media/ferrero.fe76112c.png"
+  },
+  {
+    "revision": "e01d8c49f51153d39658826fade567ad",
+    "url": "/dolcenadaa/static/media/instagram.e01d8c49.png"
+  },
+  {
+    "revision": "cdc733652ac29da8d2aaede477a7a815",
+    "url": "/dolcenadaa/static/media/logo.cdc73365.JPG"
+  },
+  {
+    "revision": "eb798d28b294cfdc98efc3be9a43479d",
+    "url": "/dolcenadaa/static/media/mainBiscotti.eb798d28.png"
+  },
+  {
+    "revision": "903b32852d8c37d6c85821b50e347c19",
+    "url": "/dolcenadaa/static/media/mainCookies.903b3285.JPG"
+  },
+  {
+    "revision": "2efae9b8bcaead89d545e1c446cdf2d4",
+    "url": "/dolcenadaa/static/media/mainScones.2efae9b8.JPG"
+  },
+  {
+    "revision": "712c801ea4fa3627030680789903267b",
+    "url": "/dolcenadaa/static/media/mars.712c801e.png"
+  },
+  {
+    "revision": "2b73f965f196abe11b04c4131b8ba4b8",
+    "url": "/dolcenadaa/static/media/miniegg.2b73f965.png"
+  },
+  {
+    "revision": "ad7f57cdf6d08a0dfb67366b0b2b77c9",
+    "url": "/dolcenadaa/static/media/nutella.ad7f57cd.png"
+  },
+  {
+    "revision": "632683e4e09fd4e43181ddb11dd1cd9d",
+    "url": "/dolcenadaa/static/media/nutellaGodess.632683e4.png"
+  },
+  {
+    "revision": "e13461be5f363b1a47839933361dc412",
+    "url": "/dolcenadaa/static/media/oreo.e13461be.png"
+  },
+  {
+    "revision": "aa018ba6a858bd6029289366dfcfddec",
+    "url": "/dolcenadaa/static/media/oreoBdayCake.aa018ba6.png"
+  },
+  {
+    "revision": "7de5aa72c04767233f3ef1047adf6cab",
+    "url": "/dolcenadaa/static/media/pastaschio.7de5aa72.png"
+  },
+  {
+    "revision": "1184d4c9ac4ca5e18739974246461a4b",
+    "url": "/dolcenadaa/static/media/profile.1184d4c9.png"
+  },
+  {
+    "revision": "d027f80c6af0cb2a61c3da5cacca9e08",
+    "url": "/dolcenadaa/static/media/reese.d027f80c.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/dolcenadaa/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/dolcenadaa/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/dolcenadaa/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/dolcenadaa/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2ac058dc0f0890c3434c27a595947c68",
+    "url": "/dolcenadaa/static/media/storytime.2ac058dc.png"
+  },
+  {
+    "revision": "f84995578f3385960e233438e74a769a",
+    "url": "/dolcenadaa/static/media/strawberryPoptart.f8499557.png"
+  },
+  {
+    "revision": "4aa1e3b0fc46b146331c5a9530b7dd3c",
+    "url": "/dolcenadaa/static/media/walnutChocolateChip.4aa1e3b0.png"
+  },
+  {
+    "revision": "bb30843c6ba26cca04f782eb003738f6",
+    "url": "/dolcenadaa/static/media/xoxo.bb30843c.png"
+  }
+]);
