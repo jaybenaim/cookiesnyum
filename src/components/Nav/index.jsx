098416720b91cb8ed0a726a938d76d9357193cf7
jaybenaim <benaimjacob@gmail.com>
2020-04-08 14:06:38 -0400

    30 price reshapped box: now sized to fit for mobile

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,13 +4,10 @@ import "../../assets/stylesheets/nav.css";
 import { Link, withRouter } from "react-router-dom";
 import Auth from "../Auth";
 import { filterGallery } from "../../redux/actions/galleryActions";
-import { Icon, Navbar, NavItem, Divider, Dropdown } from "react-materialize";
+import { Icon, Navbar } from "react-materialize";
 import { connect } from "react-redux";
 
 const Nav = props => {
-  const handleOpen = filter => {
-    props.filterGallery(filter);
-  };
   return (
     <Navbar
       alignLinks="right"
@@ -40,67 +37,11 @@ const Nav = props => {
         {" "}
         Home{" "}
       </Link>
-      {window.innerWidth >= 451 ? (
-        <Link to="/products" className="sidenav-close">
-          Products
-        </Link>
-      ) : (
-        <Dropdown
-          id="Dropdown_6"
-          options={{
-            alignment: "left",
-            autoTrigger: true,
-            closeOnClick: true,
-            constrainWidth: false,
-            container: false,
-            coverTrigger: false,
-            hover: false,
-            inDuration: 150,
-            outDuration: 250
-          }}
-          trigger={
-            <div className="sidenav--dropdown-trigger">
-              Products<Icon>arrow_drop_down</Icon>
-            </div>
-          }
-        >
-          <Link
-            to="products"
-            className="sidenav-close"
-            onClick={() => handleOpen("cookie")}
-          >
-            Cookies
-          </Link>
+      <Link to="/products" className="sidenav-close">
+        Products
+      </Link>
 
-          <Divider />
-          <Link
-            to="products"
-            className="sidenav-close"
-            onClick={() => handleOpen("scone")}
-          >
-            Scones
-          </Link>
-          <Divider />
-
-          <Link
-            to="products"
-            className="sidenav-close"
-            onClick={() => handleOpen("biscotti")}
-          >
-            Biscotti
-          </Link>
-          <Divider />
-
-          <Link
-            to="products"
-            className="sidenav-close"
-            onClick={() => handleOpen("all")}
-          >
-            All Products
-          </Link>
-        </Dropdown>
-      )}
-      <NavItem href="/dolcenada" className="sidenav-logo">
+      <Link to="/" className="sidenav-logo sidenav-close">
         {" "}
         <img
           src={logo}
@@ -108,7 +49,7 @@ const Nav = props => {
           height={140}
           width={140}
         />
-      </NavItem>
+      </Link>
 
       <Auth />
     </Navbar>
