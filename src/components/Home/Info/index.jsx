8f558f1a9f8066c6123e80d1fbc7ab63695638ce
jaybenaim <benaimjacob@gmail.com>
2020-04-09 17:19:17 -0400

    replaced template photo with client photos

--- src/components/Home/Info/index.jsx
+++ src/components/Home/Info/index.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import multiCookie from "../../../assets/images/footerImg/multiCookie.JPG";
 import instagram from "../../../assets/images/instagram.png";
 import pinkPhone from "../../../assets/images/pinkPhone.png";
+import boxCentered from "../../../assets/images/boxCentered.png";
+import boxCentered1 from "../../../assets/images/boxCentered1.png";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+
 import "../../../assets/stylesheets/info.css";
 
 const Info = () => {
@@ -12,25 +15,44 @@ const Info = () => {
       <div className="info-content">
         <h3 className="primary-font">Business Info</h3>
         <div className="secondary-font">
-          <p>Based in Toronto, ON</p>
+          <p>Based in Vaughan, ON</p>
           <p>
             <strong>
               Please give at least 7 days notice for any order placed
             </strong>
           </p>
           <em>Rushed orders available for additional charge</em>
-
-          <div>
+        </div>
+        <div className="info-content--image-grid secondary-font">
+          <img
+            className="info-content--left-image"
+            src={boxCentered1}
+            width="120px"
+            height="120px"
+            alt="chocolate scone with melted chocolate inside showing"
+          />
+          <div className="info-content--hours">
             <strong>Hours:</strong>
             <p>
               Monday - Friday <br />
               9am - 6pm
             </p>
-            <a href="tel:4162743820">
-              <img src={pinkPhone} alt="phone" className="phone-icon" />
-              416 274 3820
-            </a>
+            <div className="info-content--phone">
+              <strong>Phone:</strong>
+
+              <a href="tel:4162743820">
+                <img src={pinkPhone} alt="phone" className="phone-icon" />
+                416 274 3820
+              </a>
+            </div>
           </div>
+          <img
+            className="info-content--right-image"
+            src={boxCentered}
+            width="150px"
+            height="140px"
+            alt="double dolce cookie melted in two halves dripping caramel"
+          />
         </div>
 
         <div className="social-icons">
@@ -40,6 +62,7 @@ const Info = () => {
           </a>
         </div>
       </div>
+      {/* TODO:// move to footer */}
       <img
         src={multiCookie}
         alt="profile of owners"
@@ -50,8 +73,8 @@ const Info = () => {
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
-  errors: state.errors
+  errors: state.errors,
 });
 export default withRouter(connect(mapStateToProps, {})(Info));
