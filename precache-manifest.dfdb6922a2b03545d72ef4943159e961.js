af5221bd134cd96c6e366ecbc8e1ecc8b2f85297
jaybenaim <benaimjacob@gmail.com>
2020-04-14 15:54:41 -0400

    Updates

--- precache-manifest.dfdb6922a2b03545d72ef4943159e961.js
+++ precache-manifest.dfdb6922a2b03545d72ef4943159e961.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7a75248014ec8c46a0a9a569d444b45",
+    "revision": "d5fbec028ea41e1b1b7be9f3b7c8aa25",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.94f0bb04.chunk.css"
   },
   {
-    "revision": "9d018710b822263c3a5d",
+    "revision": "b1ea030dbd32db06c286",
     "url": "/static/css/main.8409352d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.df898a1c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9d018710b822263c3a5d",
-    "url": "/static/js/main.017758c3.chunk.js"
+    "revision": "b1ea030dbd32db06c286",
+    "url": "/static/js/main.1d115dc2.chunk.js"
   },
   {
     "revision": "1d34a7c3a2afae821ef9",
