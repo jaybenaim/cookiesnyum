3a0def6fa5347237bcff5955b55f515b5a9528b8
jaybenaim <benaimjacob@gmail.com>
2020-04-16 15:44:45 -0400

    login style desktop

--- src/components/Cart/CheckoutForm/ConfirmationModal/ConfirmationModalContent/index.jsx
+++ src/components/Cart/CheckoutForm/ConfirmationModal/ConfirmationModalContent/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { sendEmail } from "../../../../../redux/actions/checkoutActions";
 import { connect } from "react-redux";
 import { formatPrice } from "../../../../../helpers/index";
