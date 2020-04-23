d1fe0b53a888651caac2ba31d20a9e7694671bb7
jaybenaim <benaimjacob@gmail.com>
2020-04-23 13:55:09 -0400

    added primary color to background for answer

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -6,19 +6,14 @@ import "../../assets/stylesheets/faq.css";
 const Faq = () => {
   const questionElements = questions.map((question, i) => {
     return (
-      <CollapsibleItem
-        expanded={false}
-        header={question.q}
-        icon={<Icon>directions_run</Icon>}
-        node="div"
-        key={i}
-      >
+      <CollapsibleItem expanded={false} header={question.q} node="div" key={i}>
         {question.a}
       </CollapsibleItem>
     );
   });
   return (
     <div className="container faq-container">
+      <h1 className="primary-font">Frequently Asked Questions</h1>
       <Collapsible accordion>{questionElements}</Collapsible>
     </div>
   );
--- src/components/Faq/questions.js
+++ src/components/Faq/questions.js
@@ -1,7 +1,79 @@
+import React from "react";
+
 const questions = [
   {
     q: "How much for rushed orders?",
     a: "Depending on the order size $10-$20",
+  },
+  {
+    q: "How big are Dolce Nada cookies?",
+    a:
+      "We believe, the BIGGER the BETTER! Therefore, our handcrafted cookies range anywhere from 4oz-5oz",
+  },
+  {
+    q: "Where are you located?",
+    a:
+      "Dolce Nada is an online bakery therefore, we do not have a brick and mortar store…YET!",
+  },
+  {
+    q: "Do you have pickup locations?",
+    a:
+      "Yes, we offer two free pickup locations. One is located in Vaughan and one is located in Nobleton! We provide exact pickup location after your order is processed!",
+  },
+  {
+    q: "Do you Deliver?",
+    a:
+      "Yes, we do! We offer delivery all over the GTA and more for an additional cost. Delivery cost is based on location it ranges from $5-$30",
+  },
+  {
+    q: "When will I receive my order?",
+    a:
+      "We bake everything fresh to order so please give the Dolce girls 1-3 days to hand craft your special order! If your order is finished earlier, we will contact you!",
+  },
+  {
+    q: "Can I order in advance for a later date?",
+    a:
+      "Of course, we totally understand life is busy, so please feel free to order ahead and leave the requested date in the order form. If for some reason we can’t accommodate you we will reach out!",
+  },
+  {
+    q: "There is so many flavours, I can’t choose, what do you suggest?",
+    a:
+      "We totally understand, that’s why we created our surprise box where the Dolce girls pick the assortment for you!",
+  },
+  {
+    q: "How long will my cookies last once I receive them?",
+    a:
+      "Great question! Dolce Nada cookies have a shelf life of 1.5 weeks in our packaging. We totally get that you cannot finish 12 cookies that quick so we are happy to say that you can freeze our cookies for up to 5 months!",
+  },
+  {
+    q: "How long will my scones last once I receive them?",
+    a:
+      "Great question! Dolce Nada scones have a shelf life of 1.5 weeks in our packaging. You can also freeze our scones for up to 5 months!",
+  },
+  {
+    q: "Cookie care…can I reheat my cookies?",
+    a:
+      "YES, we recommend reheating our cookies in your over set to 350 degrees for about 5 minuets, this allows to have an oven fresh Dolce Nada cookie ☺",
+  },
+  {
+    q: "Scone care… can I reheat my scones?",
+    a:
+      "YES, we recommend reheating our scones in the oven at 300 degrees for about 4-7 minuets for a soft and flaky Dolce Nada scone",
+  },
+  {
+    q: "Do you offer nut free options?",
+    a:
+      "We have many cookies & scones without nuts however, Dolce Nada is not a nut free facility!",
+  },
+  {
+    q: "Do you offer catering?",
+    a:
+      "We totally do! We cater weddings, small to mid-size events and all sorts of birthday parties! Email us @ dolcenadaa@gmail.com and someone from our special events team will reach out",
+  },
+  {
+    q: "Scone care… can I reheat my scones?",
+    a:
+      "YES, we recommend reheating our scones in the oven at 300 degrees for about 4-7 minuets for a soft and flaky Dolce Nada scone",
   },
 ];
 export default questions;
